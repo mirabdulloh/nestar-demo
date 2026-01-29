731a75ca83899f90e9ed620a741fbf1d7464cacd
mirabdulloh <tursunovmirabdulla@gmail.com>
2026-01-28 17:07:30 -0800

    feat: publish basic layout components

--- libs/components/layout/LayoutBasix.tsx
+++ libs/components/layout/LayoutBasix.tsx
@@ -1,25 +1,45 @@
 import { Stack } from "@mui/material";
-import brown from "@mui/material/colors/brown";
-import green from "@mui/material/colors/green";
 import Head from "next/head";
-import { Component } from "react";
+import Top from "../Top";
+import Footer from "../Footer";
 
 const withLayoutBasic = (Component: any) => {
   return (props: any) => {
-   return(
-     <>
-      <Head>
-        <title>Nestar</title>
-      </Head>
-      <Stack id="pc-wrap">
-        <Stack sx={{ background: green[300] }}>Header Basic</Stack>
-        <Stack id="main">
-          <Component {...props} />
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+
+        <Stack id="pc-wrap">
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
+
+          <Stack
+            className={"header-basic"}
+            style={{
+              backgroundImage: `url(/img/banner/properties.png)`,
+              backgroundSize: "cover",
+              boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+            }}
+          >
+            <Stack className={"container"}>
+              <strong>Property Search</strong>
+              <span>We are glad to see you again!</span>
+            </Stack>
+          </Stack>
+
+          <Stack id={"main"}>
+            <Component {...props} />
+          </Stack>
+
+          <Stack id={"footer"}>
+            <Footer />
+          </Stack>
         </Stack>
-        <Stack sx={{ background: brown[300] }}>Footer</Stack>
-      </Stack>
-    </>
-   )
+      </>
+    );
   };
 };
 
