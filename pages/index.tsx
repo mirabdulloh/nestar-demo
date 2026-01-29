ba4452f1ae6b6b30080c7ed4fd768789e2128785
mirabdulloh <tursunovmirabdulla@gmail.com>
2026-01-28 20:04:59 -0800

    feat: published homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,25 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import Top from "@/libs/components/Top";
 import { Box, Container, Stack } from "@mui/material";
 import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TrendProperties from "@/libs/components/HomePage/TrendProperties";
+import PopularProperties from "@/libs/components/HomePage/PopularProperties";
+import Advertisement from "@/libs/components/HomePage/Advertisement";
+import TopProperties from "@/libs/components/HomePage/TopProperties";
+import TopAgents from "@/libs/components/HomePage/TopAgents";
 
 const Home: NextPage = () => {
   return (
-    <Stack className="home-page">
-      <Stack>
-        <Stack className="container">Trend Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Popular Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Advertisement</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Agents</Stack>
-      </Stack>
+    <Stack className="homepage">
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
