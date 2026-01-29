e4aecc45a77ae96773da09c45eaac1b5fcec794f
mirabdulloh <tursunovmirabdulla@gmail.com>
2026-01-28 16:15:11 -0800

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useEffect, useState } from "react";
+import "../scss/app.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
 
